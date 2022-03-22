147bc12fa258414628f234fa0e6664d2ca0d6978
LizaAgey <agey.eliz@gmail.com>
2022-03-22 21:30:04 +0300

    localStorage added

--- #3 Recipe app/script.js
+++ #3 Recipe app/script.js
@@ -2,8 +2,9 @@ const meals = document.getElementById('meals')
 const globalMeals = document.getElementById('global-meals')
 let inputSearch = document.getElementById("text-to-search")
 const searchBtn = document.getElementById("search")
+// const heartBtn = document.getElementById("")
 
-
+addMealsToFav()
 
 // R A N D O M
 getRandomMeal()
@@ -24,15 +25,15 @@ function addRandomMeal(mealData) {
    <div class="meal-header">
         <button class="random-header" id="random-btn">Random meal</button> 
         <div class="random-meal">
-            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}" id="img-meal">
         </div>
         <div class="meal-name">
             <div class="random-name">
-                <h4>${mealData.strMeal}</h4>
+                <h4 id="meal-name">${mealData.strMeal}</h4>
             </div>
             <div class="fav-btn active">
                 <button class="heart-button" onclick="changeIcon()">
-                    <i class="fa-regular fa-heart" id="change-icon"></i>
+                    <i class="fa-regular fa-heart change-icon" id="${mealData.idMeal}"></i>
                 </button>
             </div>
         </div>
@@ -84,15 +85,15 @@ function replaceExistingMeal(mealData) {
     meal.innerHTML = `
    <div class="meal-header">
         <div class="random-meal">
-            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}" id="img-meal">
         </div>
         <div class="meal-name">
             <div class="random-name">
-                <h4>${mealData.strMeal}</h4>
+                <h4 id="meal-name">${mealData.strMeal}</h4>
             </div>
             <div class="fav-btn active">
                 <button class="heart-button" onclick="changeIcon()">
-                    <i class="fa-regular fa-heart" id="change-icon"></i>
+                    <i class="fa-regular fa-heart change-icon" id="${mealData.idMeal}"></i>
                 </button>
             </div>
         </div>
@@ -105,21 +106,101 @@ function replaceExistingMeal(mealData) {
 
 
 function changeIcon() {
-    const iconClass = document.getElementById("change-icon")
-    if (iconClass.className == "fa-regular fa-heart") {
-        iconClass.className = "fa-solid fa-heart"
-        // addMealToLocalStorage()
+    const heartBtn = document.querySelector(".change-icon")
+    if (heartBtn.className == "fa-regular fa-heart change-icon") {
+        heartBtn.className = "fa-solid fa-heart change-icon"
+        addMealToLocalStorage()
+
     } else {
-        iconClass.className = "fa-regular fa-heart"
-        // removeFromLS()
+        heartBtn.className = "fa-regular fa-heart change-icon"
+        removeFromLS()
     }
-    return iconClass.className
+    return heartBtn.className
+
+}
+
+// L O C A L    S T O R A G E
+
+function addMealToLocalStorage() {
+    const heartBtnID = document.querySelector('.change-icon').id
+    const mealIds = getMealsLS();
+
+    localStorage.setItem("mealIds", JSON.stringify([...mealIds, new String(heartBtnID)]))
+
+    // let arrayWithIdsFromLS = JSON.parse(localStorage.getItem('mealIds')) // receive array with all IDs in LS
+
+    // arrayWithIdsFromLS.forEach((id) => {
+        const listOfFavMeals = document.getElementById("meal-ul-list")
+        listOfFavMeals.innerHTML = '';
+        addMealsToFav()
+
+    // });
+
+
+
+
+}
+
+function removeFromLS() {
+    const heartBtnID = document.querySelector('.change-icon').id
+    const mealIds = getMealsLS();
+
+    localStorage.setItem("mealIds", JSON.stringify(mealIds.filter((id) => id !== heartBtnID)));
+    const listOfFavMeals = document.getElementById("meal-ul-list")
+        listOfFavMeals.innerHTML = '';
+        addMealsToFav()
+
+}
+
+function getMealsLS() {
+    const mealIds = JSON.parse(localStorage.getItem("mealIds"));
+
+    return mealIds === null ? [] : mealIds;
+}
+
+// GET MEAL by ID
+async function getMealById(id) {
+    const resp = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id
+    );
+
+    const respData = await resp.json();
+    const meal = respData.meals[0];
+    // console.log(meal)
+
+    return meal; //receive object with Meal data
+}
+
+
+// ADD  from LS to FAVORITES
+
+function addMealsToFav() {
+
+    let meaLIdsLS = getMealsLS()
+    meaLIdsLS.forEach(id => {
+        let mealData = getMealById(id)
+
+        mealData = mealData.then(function (mealData) {
+
+
+            const listOfFavMeals = document.getElementById("meal-ul-list")
+            let favMeal = document.createElement('li');
+
+            favMeal.innerHTML = `       
+             <img src="${mealData.strMealThumb}" alt="">
+            <span>${mealData.strMeal}</span>`
+
+            listOfFavMeals.append(favMeal);
+        })
+
+    })
 
 }
 
 
-//local storage: you would keep a key on the user’s computer and read it out when the user returns.
+// const scrollContainer = document.querySelector("meal-ul-list");
 
-//Если в метод getItem() интерфейса Storage передать ключ в качестве параметра, то метод вернёт значение, лежащее в хранилище по указанному ключу.
-//storage.setItem(название Ключа, значение Ключа);
-
+// scrollContainer.addEventListener("wheel", (evt) => {
+//     evt.preventDefault();
+//     scrollContainer.scrollLeft += evt.deltaY;
+// })
